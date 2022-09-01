18b5a7c55bf284ec9184f7641cf5bae5679f0ef4
CindyCarr <cindyparedes001@gmail.com>
2022-08-31 20:01:12 -0400

    Code Changed to Even Numbers Only 1-100

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
 
     public static void main(String[] args) {
-        for (int i=1; i<=100; i++){
+        for (int i=2; i<=100; i++){
         System.out.print(i+" ");
 
         }
